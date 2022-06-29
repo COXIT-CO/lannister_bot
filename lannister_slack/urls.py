5488f1407d218bd13ae3b6fdb58787b7e900683b
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-29 23:41:11 +0300

    Add skeleton urls

--- lannister_slack/urls.py
+++ lannister_slack/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from lannister_slack.views import SlackEventView
-
+from lannister_slack.views import SlackEventView, BonusRequestViewSet
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path("events", SlackEventView.as_view(), name="slack-events"),
 ]
+
+router = DefaultRouter()
+router.register("bonus-requests", BonusRequestViewSet)
+urlpatterns += router.urls
