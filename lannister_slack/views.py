fc5acb121cd0e192913ecc51bab248eec4da6076
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-30 23:29:03 +0300

    Add couple of slack command handlers

--- lannister_slack/views.py
+++ lannister_slack/views.py
@@ -7,6 +7,7 @@ from lannister_slack.slack_client import slack_client
 from lannister_slack.models import BonusRequest
 from lannister_slack.serializers import BonusRequestSerializer
 from lannister_slack.utils import prettify_json, BotMessage
+from lannister_auth.models import LannisterUser, Role
 
 
 @api_view(["POST"])
@@ -33,22 +34,30 @@ class SlackEventView(APIView):
         """
         print(prettify_json(request.data))
         event = request.data["event"]
+        channel_id = event.get("channel")
+        user_id = event.get("user_id")
+        text = event.get("text")
+
         if event["type"] == "message":
-            if BOT_ID != event["user"]:
-                slack_client.chat_postMessage(
-                    channel=event["channel"], text=event["text"]
-                )
+            if user_id and BOT_ID != event["user"]:
+                slack_client.chat_postMessage(channel=channel_id, text=text)
         return Response(status=status.HTTP_200_OK)
 
 
-class RegisterUserThroughSlackView(APIView):
+"""
+TODO: refactor views by making base view (CommandView or smtng) which will store all the channel/user/text data and inherits from APIView or any other generic
+"""
+
+
+class RegisterUserCommandView(APIView):
     """
     Handler for '/register command'
     View is used for testing of slack commands api
     """
 
     def post(self, request):
-        # print(prettify_json(request.data))
+        # query db and check if user has slack id, respond with denied if true, respond registered if false
+        print(prettify_json(request.data))
         username = request.data.get("user_name", None)
         channel = request.data.get("channel_id", None)
         bot_message = BotMessage(channel, username)
@@ -56,7 +65,7 @@ class RegisterUserThroughSlackView(APIView):
         return Response(status=status.HTTP_200_OK)
 
 
-class ChooseActionView(APIView):
+class ChooseActionCommandView(APIView):
     """
     Handler for '/actions' command
     Should suggest using '/list-requests' or '/new-request' to the user
@@ -74,8 +83,9 @@ class ChooseActionView(APIView):
         return Response(status=status.HTTP_200_OK)
 
 
-class ListRequestsView(APIView):
+class ListRequestsCommandView(APIView):
     def post(self, request):
+        print(request.data)
         user_id = request.data.get("user_id", None)
         if not user_id:
             # raise some exception
@@ -90,6 +100,66 @@ class ListRequestsView(APIView):
         )
         slack_client.chat_postMessage(**bot_message.list_requests_response_markdown())
         return Response(status=status.HTTP_200_OK)
+
+
+class NewRequestCommandView(APIView):
+    def post(self, request):
+        print(prettify_json(request.data))
+        username = request.data.get("user_name", None)
+        channel = request.data.get("channel_id", None)
+        # text = request.data.get("text", None)
+        bot_message = BotMessage(channel, username)
+        slack_client.chat_postMessage(**bot_message.new_request_response_markdown())
+        return Response(status=status.HTTP_200_OK)
+
+
+class EditRequestCommandView(APIView):
+    def post(self, request):
+        print(prettify_json(request.data))
+        username = request.data.get("user_name", None)
+        channel = request.data.get("channel_id", None)
+        text = request.data.get("text", None)
+        # check permissions
+        try:
+            bonus_request = BonusRequest.objects.get(pk=text)
+        except Exception as e:
+            print(e)
+            slack_client.chat_postMessage(
+                channel=channel,
+                text="Invalid bonus-request id. Hint: use /edit-request <request-id>",
+            )
+        print(bonus_request)
+        bot_message = BotMessage(channel, username, collection=bonus_request)
+        slack_client.chat_postMessage(**bot_message.edit_request_response_markdown())
+        return Response(status=status.HTTP_200_OK)
+
+
+class ReviewRequestCommandView(APIView):
+    def post(self, request):
+        print(prettify_json(request.data))
+        # username = request.data.get("user_name", None)
+        channel = request.data.get("channel_id", None)
+        text = request.data.get("text", None)
+        reviewer_role = Role.objects.get(id=2)
+        reviewer = LannisterUser.objects.get(username=text)
+        if reviewer_role in reviewer.roles.all():
+            slack_client.chat_postMessage(
+                channel=channel, text=f"Assigned *{reviewer}* successfully"
+            )
+        else:
+            slack_client.chat_postMessage(
+                channel=channel, text="Reviewer with such username does not exist"
+            )
+
+        return Response(status=status.HTTP_200_OK)
+
+
+# class AddReviewerCommandView(APIView):
+#     def post(self, request):
+#         print(prettify_json(request.data))
+#         # username = request.data.get("user_name", None)
+#         channel = request.data.get("channel_id", None)
+#         text = request.data.get("text", None)
 
 
 class BonusRequestViewSet(ModelViewSet):
