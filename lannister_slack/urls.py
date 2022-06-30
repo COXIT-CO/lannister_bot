03124eb2ac6f60776b33f56251e076208a6ecc4f
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-30 17:07:00 +0300

    Add new views to handle slash commands https://api.slack.com/apps/A03MMSE5XR8/slash-commands?saved=1

--- lannister_slack/urls.py
+++ lannister_slack/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from lannister_slack.views import SlackEventView, BonusRequestViewSet
+from lannister_slack.views import (
+    SlackEventView,
+    BonusRequestViewSet,
+    RegisterUserThroughSlackView,
+    ChooseActionView,
+    ListRequestsView,
+)
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path("events", SlackEventView.as_view(), name="slack-events"),
+    path("register", RegisterUserThroughSlackView.as_view(), name="register-in-slack"),
+    path("actions", ChooseActionView.as_view(), name="choose-actions"),
+    path("list-requests", ListRequestsView.as_view(), name="list-requests"),
 ]
 
 router = DefaultRouter()
--- lannister_slack/views.py
+++ lannister_slack/views.py
@@ -6,8 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 from lannister_slack.slack_client import slack_client
 from lannister_slack.models import BonusRequest
 from lannister_slack.serializers import BonusRequestSerializer
-
-import json
+from lannister_slack.utils import prettify_json, BotMessage
 
 
 @api_view(["POST"])
@@ -17,22 +16,13 @@ def respond_to_challenge(request):
     return Response(request.data["challenge"], status=status.HTTP_200_OK)
 
 
-def to_pretty_json(data):
-    """
-    Helper function that prettifies slack event data json
-
-    Refactor to helpers/utils file later
-    """
-    return json.dumps(data, indent=4)
-
-
 # grab bot's id so he won't trigger at his own messages later
 BOT_ID = slack_client.api_call("auth.test")["user_id"]
 
 
 class SlackEventView(APIView):
     """
-    Main view that handles all events that were enabled in app's dashboard
+    Main event view that handles all events that were enabled in app's dashboard
 
     Events are coming as POST requests
     """
@@ -41,13 +31,64 @@ class SlackEventView(APIView):
         """
         Echoes user's message written into bot's private messages or #slack-app channel
         """
-        print(to_pretty_json(request.data))
+        print(prettify_json(request.data))
         event = request.data["event"]
         if event["type"] == "message":
             if BOT_ID != event["user"]:
                 slack_client.chat_postMessage(
                     channel=event["channel"], text=event["text"]
                 )
+        return Response(status=status.HTTP_200_OK)
+
+
+class RegisterUserThroughSlackView(APIView):
+    """
+    Handler for '/register command'
+    View is used for testing of slack commands api
+    """
+
+    def post(self, request):
+        # print(prettify_json(request.data))
+        username = request.data.get("user_name", None)
+        channel = request.data.get("channel_id", None)
+        bot_message = BotMessage(channel, username)
+        slack_client.chat_postMessage(**bot_message.register_response_markdown())
+        return Response(status=status.HTTP_200_OK)
+
+
+class ChooseActionView(APIView):
+    """
+    Handler for '/actions' command
+    Should suggest using '/list-requests' or '/new-request' to the user
+    Check 'user flow' slide for more
+    https://docs.google.com/presentation/d/1EqXRMvbUFbwAnEkk7jZWgyDhzom56kYO32ZZvOv8_Vw/edit#slide=id.g135a3df236f_0_49
+    """
+
+    def post(self, request):
+        print(prettify_json(request.data))
+        # parse user_id, grab his permissions and move from there
+        username = request.data.get("user_name", None)
+        channel = request.data.get("channel_id", None)
+        bot_message = BotMessage(channel=channel, username=username)
+        slack_client.chat_postMessage(**bot_message.actions_response_markdown())
+        return Response(status=status.HTTP_200_OK)
+
+
+class ListRequestsView(APIView):
+    def post(self, request):
+        user_id = request.data.get("user_id", None)
+        if not user_id:
+            # raise some exception
+            pass
+
+        username = request.data.get("user_name", None)
+        channel = request.data.get("channel_id", None)
+        bonus_requests = BonusRequest.objects.filter(creator__slack_user_id=user_id)
+        print(bonus_requests)
+        bot_message = BotMessage(
+            channel=channel, username=username, collection=bonus_requests
+        )
+        slack_client.chat_postMessage(**bot_message.list_requests_response_markdown())
         return Response(status=status.HTTP_200_OK)
 
 
