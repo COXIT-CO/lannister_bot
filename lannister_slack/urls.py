396a2d10a9db028b0999c6f15ef41ab93d7d8028
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-30 23:28:42 +0300

    Add url endpoints for commands

--- lannister_slack/urls.py
+++ lannister_slack/urls.py
@@ -2,17 +2,27 @@ from django.urls import path
 from lannister_slack.views import (
     SlackEventView,
     BonusRequestViewSet,
-    RegisterUserThroughSlackView,
-    ChooseActionView,
-    ListRequestsView,
+    RegisterUserCommandView,
+    ChooseActionCommandView,
+    ListRequestsCommandView,
+    NewRequestCommandView,
+    EditRequestCommandView,
+    ReviewRequestCommandView,
 )
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path("events", SlackEventView.as_view(), name="slack-events"),
-    path("register", RegisterUserThroughSlackView.as_view(), name="register-in-slack"),
-    path("actions", ChooseActionView.as_view(), name="choose-actions"),
-    path("list-requests", ListRequestsView.as_view(), name="list-requests"),
+    path("register", RegisterUserCommandView.as_view(), name="register-in-slack"),
+    path("actions", ChooseActionCommandView.as_view(), name="choose-actions"),
+    path("list-requests", ListRequestsCommandView.as_view(), name="list-requests"),
+    path("new-request", NewRequestCommandView.as_view(), name="new-requests"),
+    path("edit-request", EditRequestCommandView.as_view(), name="edit-request"),
+    path("review-request", ReviewRequestCommandView.as_view(), name="new-requests"),
+    # path("add-reviewer", NewRequestCommandView.as_view(), name="new-requests"),
+    # path("remove-reviewer", NewRequestCommandView.as_view(), name="new-requests"),
+    # path("history", NewRequestCommandView.as_view(), name="new-requests"),
+    # path("list-users", NewRequestCommandView.as_view(), name="new-requests"),
 ]
 
 router = DefaultRouter()
