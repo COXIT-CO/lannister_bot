c6806572f46e23fec8e0a6c2a6d43fd2544bcf55
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-30 17:06:15 +0300

    Add fields for many-to-many serialization

--- lannister_slack/serializers.py
+++ lannister_slack/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 from lannister_slack.models import BonusRequest
+from lannister_auth.serializers import UserSerializer
 
 
 class BonusRequestSerializer(ModelSerializer):
+    creator = UserSerializer(read_only=True)
+    reviewer = UserSerializer(read_only=True)
+
     class Meta:
         model = BonusRequest
-        fields = "__all__"
+        fields = [
+            "id",
+            "status",
+            "bonus_type",
+            "description",
+            "created_at",
+            "updated_at",
+            "payment_date",
+            "creator",
+            "reviewer",
+        ]
