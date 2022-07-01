7a6d3817a1ca3ebcb808bc45affd1d6203639f39
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-01 20:06:25 +0300

    Add new models to admin

--- lannister_slack/admin.py
+++ lannister_slack/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from lannister_slack.models import BonusRequest
+from lannister_slack.models import (
+    BonusRequest,
+    BonusRequestsHistory,
+    BonusRequestStatus,
+)
 
 # Register your models here.
 admin.site.register(BonusRequest)
+admin.site.register(BonusRequestsHistory)
+admin.site.register(BonusRequestStatus)
