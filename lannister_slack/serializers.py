a1e6c43eaf68305863720006d0b2e84320ef9688
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-07-02 19:28:12 +0300

    Created serializers for different actions by permissions

--- lannister_slack/serializers.py
+++ lannister_slack/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework.serializers import ModelSerializer
 from lannister_slack.models import BonusRequest
 
+#
+# class BonusRequestSerializer(ModelSerializer):
+#     class Meta:
+#         model = BonusRequest
+#         fields = "__all__"
+#
 
-class BonusRequestSerializer(ModelSerializer):
+"""
+For per-field permissions(update method)
+"""
+
+class BonusRequestAdminSerializer(ModelSerializer):
     class Meta:
         model = BonusRequest
         fields = "__all__"
+        read_only_fields = ('created_at', 'updated_at',)
+
+class BonusRequestRewieverSerializer(ModelSerializer):
+    class Meta:
+        model = BonusRequest
+        fields = "__all__"
+        read_only_fields = ('creator', 'reviewer', 'description', 'created_at', 'updated_at',)
+
+class BonusRequestBaseSerializer(ModelSerializer):
+    class Meta:
+        model = BonusRequest
+        fields = "__all__"
+        read_only_fields = ('creator', 'created_at', 'updated_at', 'price_usd', 'payment_date', )
