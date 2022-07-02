b7a878e603bcfe65a91fe7092182598162f51fd0
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-07-02 19:27:16 +0300

    Add new permission class

---
+++ lannister_slack/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsUser(BasePermission):
+    allowed_methods = ["GET", "POST", "PUT", "PATCH"]
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in self.allowed_methods:
+            return True
+        return obj.username == request.user.username
