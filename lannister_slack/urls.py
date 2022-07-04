a92f41ede1f875e5c220c410b102425df0b399d7
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-07-04 23:40:57 +0300

    Add endpoints

--- lannister_slack/urls.py
+++ lannister_slack/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path
-from lannister_slack.views import SlackEventView, BonusRequestViewSet
+from django.urls import path, include
+"""When slack needed uncomment"""
+# from lannister_slack.views import SlackEventView, BonusRequestViewSet
+from lannister_slack.views import BonusRequestViewSet, HistoryRequestViewSet
 from rest_framework.routers import DefaultRouter
 
+
+"""When slack needed uncomment"""
 urlpatterns = [
-    path("events", SlackEventView.as_view(), name="slack-events"),
+#     path("events", SlackEventView.as_view(), name="slack-events"),
+    path("requests/", BonusRequestViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path("requests/<pk>", BonusRequestViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'patch': 'partial_update'})),
+    path("requests/history/<pk>", HistoryRequestViewSet.as_view({'get': 'retrieve'})),
+    path("requests/history/", HistoryRequestViewSet.as_view({'get': 'list'})),
 ]
 
-router = DefaultRouter()
-router.register("bonus-requests", BonusRequestViewSet)
-urlpatterns += router.urls
+# router = DefaultRouter()
+# router.register("bonus-requests", BonusRequestViewSet)
+# urlpatterns += router.urls
+
