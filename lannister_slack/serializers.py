6400a50dc1358d70ded02387767e202d778e9123
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-07-04 23:39:49 +0300

    Add serializer for history model

--- lannister_slack/serializers.py
+++ lannister_slack/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from lannister_slack.models import BonusRequest
+from lannister_slack.models import BonusRequest, BonusRequestsHistory
 
 #
 # class BonusRequestSerializer(ModelSerializer):
@@ -29,3 +29,15 @@ class BonusRequestBaseSerializer(ModelSerializer):
         model = BonusRequest
         fields = "__all__"
         read_only_fields = ('creator', 'created_at', 'updated_at', 'price_usd', 'payment_date', )
+
+class BonusRequestHistorySerializer(ModelSerializer):
+    class Meta:
+        model = BonusRequestsHistory
+        fields = ('status', 'date',)
+        read_only_fields = ('status', 'date',)
+
+class FullHistorySerializer(ModelSerializer):
+    history_requests = BonusRequestHistorySerializer(many=True, read_only=True)
+    class Meta:
+        model = BonusRequest
+        fields = ['id', 'creator', 'reviewer', 'bonus_type', 'price_usd', 'payment_date', 'history_requests']
