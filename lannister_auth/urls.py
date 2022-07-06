952099935ec2081d9d8aa47ec2948c1827738835
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-06 18:42:05 +0300

    Purge djoser out from the project

--- lannister_auth/urls.py
+++ lannister_auth/urls.py
@@ -2,17 +2,15 @@ from rest_framework.routers import DefaultRouter
 from lannister_auth.views import UserViewSet, RegisterUser
 from rest_framework_simplejwt import views
 
-from django.urls import path, include, re_path
+from django.urls import path, re_path
 
 urlpatterns = [
-    path("", include("djoser.urls")),
     re_path(r"^token/?", views.TokenObtainPairView.as_view(), name="jwt-obtain-token"),
     re_path(r"^refresh/?", views.TokenRefreshView.as_view(), name="jwt-refresh"),
     re_path(r"^verify/?", views.TokenVerifyView.as_view(), name="jwt-verify"),
-    # path("", include("djoser.urls.jwt")),
     path("register/", RegisterUser.as_view(), name="register-user"),
 ]
 
 router = DefaultRouter()
-router.register("lannister-users", UserViewSet)
+router.register("users", UserViewSet)
 urlpatterns += router.urls
