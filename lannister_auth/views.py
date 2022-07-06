65106172e272404afbff15f5d3a4b90b3c4cb319
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-06 12:18:12 +0300

    Add registration view for unregistered user. TODO:check comment

--- lannister_auth/views.py
+++ lannister_auth/views.py
@@ -1,6 +1,40 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView
 from lannister_auth.serializers import UserSerializer
-from lannister_auth.models import LannisterUser
+from lannister_auth.models import LannisterUser, Role
+
+
+class RegisterUser(CreateAPIView):
+    """
+    View to allow user to register himself as worker by default.
+    TODO: add different view for admin to register reviewer through API
+    or check if jwt token was provided by the admin user => verify permission => perform register
+    """
+
+    serializer_class = UserSerializer
+    queryset = LannisterUser.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        user_data = request.data
+        user = LannisterUser.objects.create_user(
+            email=user_data.get("email"),
+            username=user_data.get("username"),
+            password=user_data.get("password"),
+            first_name=user_data.get("first_name"),
+            last_name=user_data.get("last_name"),
+        )
+        if not user_data.get("roles"):
+            user.roles.add(Role.objects.get(id=1))
+            user.save()
+        # serializer = self.serializer_class(user)
+        return Response(
+            {
+                "response": "Registered successfully, please contact @lannister-bot with /register in your workspace to continue"
+            },
+            status=status.HTTP_201_CREATED,
+        )
 
 
 class UserViewSet(ModelViewSet):
