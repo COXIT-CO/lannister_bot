7f1348ee66f934e5d50219c4c87a2f4388dae034
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-06 12:17:35 +0300

    Replace djoser jwt endpoints with more readable /api/auth/token etc

--- lannister_auth/urls.py
+++ lannister_auth/urls.py
@@ -1,10 +1,16 @@
 from rest_framework.routers import DefaultRouter
-from lannister_auth.views import UserViewSet
-from django.urls import path, include
+from lannister_auth.views import UserViewSet, RegisterUser
+from rest_framework_simplejwt import views
+
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path("", include("djoser.urls")),
-    path("", include("djoser.urls.jwt")),
+    re_path(r"^token/?", views.TokenObtainPairView.as_view(), name="jwt-obtain-token"),
+    re_path(r"^refresh/?", views.TokenRefreshView.as_view(), name="jwt-refresh"),
+    re_path(r"^verify/?", views.TokenVerifyView.as_view(), name="jwt-verify"),
+    # path("", include("djoser.urls.jwt")),
+    path("register/", RegisterUser.as_view(), name="register-user"),
 ]
 
 router = DefaultRouter()
