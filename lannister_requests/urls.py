16b6f1c396f60d5b8bc3eb1521565660d4d76c38
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-07-07 13:11:56 +0300

    Add urls for Bonus Statuses

--- lannister_requests/urls.py
+++ lannister_requests/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from lannister_requests.views import BonusRequestViewSet, HistoryRequestViewSet
+from lannister_requests.views import BonusRequestViewSet, HistoryRequestViewSet, BonusRequestStatusViewSet
 from rest_framework.routers import DefaultRouter
 
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path("<pk>", BonusRequestViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'patch': 'partial_update'})),
     path("history/<pk>", HistoryRequestViewSet.as_view({'get': 'retrieve'})),
     path("history/", HistoryRequestViewSet.as_view({'get': 'list'})),
+    path("status/", BonusRequestStatusViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path("status/<pk>", BonusRequestStatusViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'patch': 'partial_update'})),
 ]
