41c9727fa0130b1427ea0f0ea2457e3065c7a0eb
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-07 21:43:21 +0300

    Add new permission to explicitly check if user ran /register in Slack's ui or has slack_user_id field not being None

--- lannister_slack/permissions.py
+++ lannister_slack/permissions.py
@@ -3,18 +3,27 @@ from rest_framework.permissions import BasePermission
 # from lannister_auth.models import LannisterUser
 
 
+class IsUnregisteredMemberOfSlackWorkspace(BaseException):
+    def has_permission(self, request, view):
+        request_from_slack = request.META.get("HTTP_USER_AGENT")
+        if request_from_slack and "Slackbot 1.0" in request_from_slack:
+            return True
+
+        if request.user:  # user sent jwt token with request
+            return True
+
+
 class IsMemberOfSlackWorkspace(BasePermission):
     message = "You're not a member of this Slack workspace, pls contact an admin"
 
     def has_permission(self, request, view):
         request_from_slack = request.META.get("HTTP_USER_AGENT")
-        if "Slackbot 1.0" in request_from_slack:
+        if request_from_slack and "Slackbot 1.0" in request_from_slack:
             print("from slack")
             return True
 
-        try:
+        if request.user:  # can be AnonymousUser which doesn't have slack_user_id field
             if request.user.slack_user_id:
                 return True
-        except AttributeError:
-            return False
-        # return False
+
+        return False
