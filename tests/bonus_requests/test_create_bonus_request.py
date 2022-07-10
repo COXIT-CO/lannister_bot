3deb8350969066f39c27e2bd997eedeebd99c51f
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-10 09:37:24 +0300

    Add simple tests for BonusRequest/BonusRequestsHistory models signals/relations

--- tests/bonus_requests/test_create_bonus_request.py
+++ tests/bonus_requests/test_create_bonus_request.py
@@ -1,11 +1,17 @@
 import pytest
-from lannister_slack.models import BonusRequestStatus
+from lannister_slack.models import BonusRequestsHistory
 
 pytestmark = [pytest.mark.django_db]
 
 
-def test_was_signal_to_create_statuses_triggered_on_top_of_created_status_by_mixer(
-    dummy_bonus_request,
-):
-    assert dummy_bonus_request.__class__.objects.values("status") is not None
-    assert len(BonusRequestStatus.objects.values("status_name")) == 5
+def test_bonus_request_created(dummy_bonus_request):
+    assert isinstance(dummy_bonus_request.id, int)
+
+
+def test_status_created(dummy_status):
+    assert isinstance(dummy_status.id, int)
+
+
+def test_history_model_created_on_bonus_request_creation(dummy_bonus_request):
+    assert isinstance(dummy_bonus_request.id, int)
+    assert BonusRequestsHistory.objects.all().count() == 1
