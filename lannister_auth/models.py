cbd4b0418079c316f316ae3ba2b323e914ee5a55
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-07-11 17:30:46 +0300

    Refactor code

--- lannister_auth/models.py
+++ lannister_auth/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.core.exceptions import ObjectDoesNotExist
-from django.db import models
 from django.db.models.signals import pre_save
 from django.utils.translation import gettext_lazy as _
 
