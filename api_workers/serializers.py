e1998e9e8efd9290b3a0b2eb85f13783ad8f8367
AleRyp <mr.aleryp@gmail.com>
2022-07-15 00:17:03 +0300

    Refactor serializers for users

--- api_workers/serializers.py
+++ api_workers/serializers.py
@@ -2,8 +2,17 @@ from rest_framework import serializers
 from lannister_auth .models import LannisterUser
 
 
-class WorkerSerializer(serializers.ModelSerializer):
+class WorkerBaseSerializer(serializers.ModelSerializer):
     class Meta:
+        # extra_kwargs = {'password': {'write_only': True}, 'groups': {'write_only': True}, 'user_permissions': {'write_only': True}}
         model = LannisterUser
-        fields = "__all__"
+        fields = ["username", "email", "first_name", "last_name", "roles"]
+        read_only_fields = ["roles"]
+
+
+class WorkerAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        # extra_kwargs = {'password': {'write_only': True}, 'groups': {'write_only': True}, 'user_permissions': {'write_only': True}}
+        model = LannisterUser
+        fields = ["username", "email", "first_name", "last_name", "roles"]
 
