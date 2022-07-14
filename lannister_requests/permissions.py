509f1173a919fc4b94890438d07af981ad3bc009
AleRyp <mr.aleryp@gmail.com>
2022-07-15 00:15:54 +0300

    Implement permissions by roles for requests

--- lannister_requests/permissions.py
+++ lannister_requests/permissions.py
@@ -1,12 +1,32 @@
 from rest_framework.permissions import BasePermission
+from lannister_auth.models import Role
+from lannister_requests.models import BonusRequestStatus
 
-"""
-Skeleton for Roles implementing
-"""
-class IsUser(BasePermission):
+
+class IsUserOrAdministrator(BasePermission):
     allowed_methods = ["GET", "POST", "PUT", "PATCH"]
 
     def has_object_permission(self, request, view, obj):
         if request.method in self.allowed_methods:
             return True
-        return obj.username == request.user.username
+        return (obj.creator == request.user and obj.status == BonusRequestStatus.objects.get(status_name="Created")) or \
+               Role.objects.get(name="Administrator") in request.user.roles.all()
+
+
+class IsAdministrator(BasePermission):
+    methods = ["POST", "PATCH", "DELETE"]
+
+    def has_permission(self, request, view):
+        if request.method in self.methods:
+            return request.user.is_staff or request.user.is_superuser or \
+                   Role.objects.get(name="Administrator") in request.user.roles.all()
+        return True
+
+
+class IsHistory(BasePermission):
+    methods = ["POST", "PUT", "PATCH", "DELETE"]
+
+    def has_permission(self, request, view):
+        if request.method in self.methods:
+            return False
+        return True
