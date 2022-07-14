ef1927a090a72c7f7bad47fee639fadf4510944e
AleRyp <mr.aleryp@gmail.com>
2022-07-15 00:18:08 +0300

    Improve ViewSets for Users

--- api_workers/views.py
+++ api_workers/views.py
@@ -1,14 +1,45 @@
-from rest_framework import generics
+from rest_framework.response import Response
+
 from lannister_auth .models import LannisterUser
-from .serializers import WorkerSerializer
+from rest_framework.viewsets import ModelViewSet
+from .serializers import WorkerBaseSerializer, WorkerAdminSerializer
+from lannister_roles.models import Role
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdministratorOrStaff
 
 
-class ListWorker(generics.ListAPIView):
+class WorkerViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdministratorOrStaff)
     queryset = LannisterUser.objects.all()
-    serializer_class = WorkerSerializer
 
+    def get_serializer_class(self):
+        if Role.objects.get(name="Administrator") in self.request.user.roles.all() or self.request.user.is_superuser:
+            return WorkerAdminSerializer
+        return WorkerBaseSerializer
 
-class DetailWorker(generics.ListAPIView):
-    queryset = LannisterUser.objects.all()
-    serializer_class = WorkerSerializer
+    def get_queryset(self):
+        if Role.objects.get(name="Administrator") in self.request.user.roles.all() or self.request.user.is_superuser:
+            return self.queryset.all()
+        return self.queryset.filter(username=self.request.user.username)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        data = {}
+        if self.request.data['roles'] != None:
+            data['roles'] = self.request.data['roles'].split(',')
+            serializer_data = self.get_serializer(instance, data=data, partial=partial)
+            serializer_data.is_valid(raise_exception=True)
+            self.perform_update(serializer_data)
+            return Response(serializer_data.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
