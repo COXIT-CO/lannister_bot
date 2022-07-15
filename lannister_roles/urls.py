41d33dd32b330e6eccc5fb28f5ef975750b27632
AleRyp <mr.aleryp@gmail.com>
2022-07-15 19:30:56 +0300

    Fix roles endpoints

--- lannister_roles/urls.py
+++ lannister_roles/urls.py
@@ -2,6 +2,6 @@ from lannister_roles.views import RoleViewSet
 from django.urls import path
 
 urlpatterns = [
-    path("roles/", RoleViewSet.as_view({'get': 'list'})),
-    path("roles/<pk>", RoleViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
+    path("", RoleViewSet.as_view({'get': 'list'})),
+    path("<pk>", RoleViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
 ]
