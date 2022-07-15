aca6d7c4dc0e5de7d02025a0010784dc62f4f5c2
AleRyp <mr.aleryp@gmail.com>
2022-07-15 19:40:21 +0300

    Add endpoints name

--- api_workers/urls.py
+++ api_workers/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import WorkerViewSet
 
 urlpatterns = [
-    path('', WorkerViewSet.as_view({'get': 'list'})),
-    path('<int:pk>/', WorkerViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'patch': 'partial_update'})),
+    path('', WorkerViewSet.as_view({'get': 'list'}), name="worker-list"),
+    path('<int:pk>/', WorkerViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'patch': 'partial_update'}),
+         name="worker-single"),
 ]
--- lannister_roles/urls.py
+++ lannister_roles/urls.py
@@ -2,6 +2,6 @@ from lannister_roles.views import RoleViewSet
 from django.urls import path
 
 urlpatterns = [
-    path("", RoleViewSet.as_view({'get': 'list'})),
-    path("<pk>", RoleViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
+    path("", RoleViewSet.as_view({'get': 'list'}), name="roles-list"),
+    path("<pk>", RoleViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'}), name="roles-single"),
 ]
