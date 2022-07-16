845d0170e3792f2948bcb314c076794f487456b4
AleRyp <mr.aleryp@gmail.com>
2022-07-16 17:44:29 +0300

    Bugfix: custom permissions fix

--- lannister_roles/permissions.py
+++ lannister_roles/permissions.py
@@ -7,5 +7,5 @@ class IsAdministratorOrStaff(BasePermission):
     def has_permission(self, request, view):
         if request.method in self.methods:
             return request.user.is_staff or request.user.is_superuser or \
-                   Role.objects.get(name="Administrator") in request.user.roles
+                   Role.objects.get(name="Administrator") in request.user.roles.all()
         return True
