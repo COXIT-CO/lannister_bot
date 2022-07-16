ef4d9b749a3f531debe213e0cac2e29f3fd14377
AleRyp <mr.aleryp@gmail.com>
2022-07-16 20:07:52 +0300

    Add id to read only field, code clean up

--- api_workers/serializers.py
+++ api_workers/serializers.py
@@ -4,15 +4,16 @@ from lannister_auth .models import LannisterUser
 
 class WorkerBaseSerializer(serializers.ModelSerializer):
     class Meta:
-        # extra_kwargs = {'password': {'write_only': True}, 'groups': {'write_only': True}, 'user_permissions': {'write_only': True}}
         model = LannisterUser
         fields = ["id", "username", "email", "first_name", "last_name", "roles"]
-        read_only_fields = ["roles"]
+        read_only_fields = ["id", "roles"]
 
 
 class WorkerAdminSerializer(serializers.ModelSerializer):
     class Meta:
-        # extra_kwargs = {'password': {'write_only': True}, 'groups': {'write_only': True}, 'user_permissions': {'write_only': True}}
         model = LannisterUser
         fields = ["id", "username", "email", "first_name", "last_name", "roles"]
+        read_only_fields = ["id"]
+
+
 
