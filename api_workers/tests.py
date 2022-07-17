d51f9990e9cb8bde69c42f062726adf7f591efa2
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-07-17 21:53:08 +0300

    Add tests for workers api

--- api_workers/tests.py
+++ api_workers/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from lannister_auth .models import LannisterUser
+from lannister_slack .models import BonusRequest
 
-# Create your tests here.
+
+class LannisterUserModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        LannisterUser.objects.create(username="alex", first_name='Oleksandr', last_name='Nykolyak')
+
+    def test_title_content(self):
+        lannisteruser = LannisterUser.objects.get(id=1)
+        expected_object_name = f'{lannisteruser.username}'
+        self.assertEquals(expected_object_name, 'alex')
+
+    def test_body_content(self):
+        lannisteruser = LannisterUser.objects.get(id=1)
+        expected_object_name = f'{lannisteruser.first_name}'
+        self.assertEquals(expected_object_name, 'Oleksandr')
+
+
--- tests/worker/test_worker_info.py
+++ tests/worker/test_worker_info.py
@@ -1,1 +1,22 @@
-import pytest
+from django.test import TestCase
+from lannister_auth .models import LannisterUser
+from lannister_slack .models import BonusRequest
+
+
+class LannisterUserModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        LannisterUser.objects.create(username="alex", first_name='Oleksandr', last_name='Nykolyak')
+
+    def test_title_content(self):
+        lannisteruser = LannisterUser.objects.get(id=1)
+        expected_object_name = f'{lannisteruser.username}'
+        self.assertEquals(expected_object_name, 'alex')
+
+    def test_body_content(self):
+        lannisteruser = LannisterUser.objects.get(id=1)
+        expected_object_name = f'{lannisteruser.first_name}'
+        self.assertEquals(expected_object_name, 'Oleksandr')
+
+
