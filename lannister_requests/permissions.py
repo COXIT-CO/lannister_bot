387b4257753b43c6d01da08cf68b019566647a3c
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-18 19:39:46 +0300

    Fix permission not working properly on DELETE method

--- lannister_requests/permissions.py
+++ lannister_requests/permissions.py
@@ -3,10 +3,12 @@ from rest_framework.permissions import BasePermission
 """
 Skeleton for Roles implementing
 """
+
+
 class IsUser(BasePermission):
     allowed_methods = ["GET", "POST", "PUT", "PATCH"]
 
     def has_object_permission(self, request, view, obj):
         if request.method in self.allowed_methods:
             return True
-        return obj.username == request.user.username
+        return obj.creator.username == request.user.username
