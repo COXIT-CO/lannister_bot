e0a767218454b59b75d7343d56b2413b9d9c28d0
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-19 11:26:23 +0300

    Add additional check to parse whether id or slack_channel_id was provided

--- backend/api_workers/views.py
+++ backend/api_workers/views.py
@@ -3,6 +3,7 @@ from lannister_auth.models import LannisterUser
 from lannister_requests.models import BonusRequest
 from .serializers import WorkerSerializer
 from lannister_requests.serializers import BonusRequestBaseSerializer
+from django.shortcuts import get_object_or_404
 
 
 class ListWorker(generics.ListCreateAPIView):
@@ -13,6 +14,14 @@ class ListWorker(generics.ListCreateAPIView):
 class DetailWorker(generics.RetrieveUpdateDestroyAPIView):
     queryset = LannisterUser.objects.all()
     serializer_class = WorkerSerializer
+
+    def get_object(self, *args, **kwargs):
+        if self.kwargs.get("pk"):
+            return get_object_or_404(LannisterUser, id=self.kwargs.get("pk"))
+
+        return get_object_or_404(
+            LannisterUser, slack_channel_id=self.kwargs.get("slack_channel_id")
+        )
 
 
 class ListWorkerRequest(generics.ListCreateAPIView):
