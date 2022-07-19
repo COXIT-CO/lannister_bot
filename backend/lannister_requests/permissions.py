394d54f926947da34cf8da12fcf3eab8ade7d839
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-19 22:25:34 +0300

    Permission rework, added processing of custom header, sent from frontend app in order to remove unnecessary complications with obtaining and refreshing jwt tokens

--- backend/lannister_requests/permissions.py
+++ backend/lannister_requests/permissions.py
@@ -1,14 +1,20 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 
 """
 Skeleton for Roles implementing
 """
 
 
-class IsUser(BasePermission):
-    allowed_methods = ["GET", "POST", "PUT", "PATCH"]
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in self.allowed_methods:
+class IsUser(IsAuthenticated):
+    def has_permission(self, request, view):
+        # custom header is neccessary for the frontend to bypass jwt token verification of IsAuthenticated class, note: possible security breach
+        if request.META.get("HTTP_X_SLACK_FRONTEND") == "slack-frontend-header":
             return True
-        return obj.creator.username == request.user.username
+
+        if not super().has_permission(request, view):
+            return False
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        # return request.creator.username == request.user.username
