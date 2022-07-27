9ed5699e71828d404c973ba97ad886029936bf81
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-27 23:12:47 +0300

    Add custom frontend header

--- frontend/lannister_slack/permissions.py
+++ frontend/lannister_slack/permissions.py
@@ -4,13 +4,12 @@ from rest_framework.permissions import BasePermission
 # from lannister_auth.models import LannisterUser
 
 
-class IsUnregisteredMemberOfSlackWorkspace(BaseException):
+class IsUnregisteredMemberOfSlackWorkspace(BasePermission):
     def has_permission(self, request, view):
+        if request.META.get("HTTP_X_SLACK_FRONTEND") == "slack-frontend-header":
+            return True
         request_from_slack = request.META.get("HTTP_USER_AGENT")
         if request_from_slack and "Slackbot 1.0" in request_from_slack:
-            return True
-
-        if request.user.is_authenticated:  # user sent jwt token with request
             return True
 
         return False
@@ -20,13 +19,10 @@ class IsMemberOfSlackWorkspace(BasePermission):
     message = "You're not a member of this Slack workspace, pls contact an admin"
 
     def has_permission(self, request, view):
+        if request.META.get("HTTP_X_SLACK_FRONTEND") == "slack-frontend-header":
+            return True
         request_from_slack = request.META.get("HTTP_USER_AGENT")
         if request_from_slack and "Slackbot 1.0" in request_from_slack:
-            print("from slack")
             return True
-
-        if request.user.is_authenticated:
-            if request.user.slack_user_id:
-                return True
 
         return False
