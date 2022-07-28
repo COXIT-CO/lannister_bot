d9e03d00ebb8c91c94fb73a729bf92ffea84ecb6
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-28 18:22:55 +0300

    Remove redundant prints

--- frontend/lannister_slack/slack_client.py
+++ frontend/lannister_slack/slack_client.py
@@ -1,10 +1,8 @@
 import os
 from slack_sdk import WebClient
 
-print(os.environ.get("GITHUB_WORKFLOW"))
 if os.environ.get("GITHUB_WORKFLOW"):
     bot_user_token = os.environ.get("BOT_USER_OAUTH_TOKEN")
-    print("bot token:" + f"{bot_user_token}")
 else:
     bot_user_token = os.getenv("BOT_USER_OAUTH_TOKEN")
 
