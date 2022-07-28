390cef9fbeb4b5490a971aae28c8f27f73e4a4b6
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-28 18:20:21 +0300

    Add new url endpoints Add names for reverse()

--- backend/api_workers/urls.py
+++ backend/api_workers/urls.py
@@ -11,8 +11,15 @@ urlpatterns = [
     # 'workers/' should've been renamed in root to distinguish whether url is returning single object or collection
     path("list/", ListWorkers.as_view()),
     path("list/<str:reviewers>/", ListWorkers.as_view()),
-    path("detail/<int:pk>/", DetailWorker.as_view()),
-    path("detail/<str:username>/", DetailWorker.as_view()),
+    path("detail/<int:pk>/", DetailWorker.as_view(), name="worker-detail-id"),
+    path(
+        "detail/<str:username>/", DetailWorker.as_view(), name="worker-detail-username"
+    ),
+    path(
+        "detail/<str:channel_id>",
+        DetailWorker.as_view(),
+        name="worker-detail-channel-id",
+    ),
     path("request/", ListWorkerRequest.as_view()),
     path("request/<int:pk>/", DetailWorkerRequest.as_view()),
 ]
