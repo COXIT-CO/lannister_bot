e7780d617c4327f8755728bb60b059a2c38da0d5
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-28 14:05:55 +0300

    CI debug

--- frontend/lannister_slack/slack_client.py
+++ frontend/lannister_slack/slack_client.py
@@ -3,6 +3,7 @@ from slack_sdk import WebClient
 
 if os.environ.get("GITHUB_WORKFLOW"):
     bot_user_token = os.environ.get("BOT_USER_OAUTH_TOKEN")
+    print("bot token:" + f"{bot_user_token}")
 else:
     bot_user_token = os.getenv("BOT_USER_OAUTH_TOKEN")
 
