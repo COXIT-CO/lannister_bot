7f3152cc1acb35b575023a38833b2824418606e8
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-28 14:14:26 +0300

    Add debug lines

--- frontend/lannister_slack/slack_client.py
+++ frontend/lannister_slack/slack_client.py
@@ -1,6 +1,7 @@
 import os
 from slack_sdk import WebClient
 
+print(os.environ.get("GITHUB_WORKFLOW"))
 if os.environ.get("GITHUB_WORKFLOW"):
     bot_user_token = os.environ.get("BOT_USER_OAUTH_TOKEN")
     print("bot token:" + f"{bot_user_token}")
