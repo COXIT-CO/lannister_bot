6568f4f50b28603841c24fa9fb96573f36840ed5
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-28 18:22:04 +0300

    Remove redundant imports PEP8 styling

--- backend/lannister_roles/models.py
+++ backend/lannister_roles/models.py
@@ -1,15 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager
 
 
 class Role(models.Model):
-    objects = BaseUserManager()
     id = models.AutoField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=20)  # get_user_display()
     description = models.CharField(max_length=128, null=True)
 
     def __str__(self):
         return self.name
-
-
-
