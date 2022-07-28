b41b0b6f4e16b726b2af5ae1ab8244c58a568754
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-28 18:36:53 +0300

    Refactor test with role name instead of id

--- backend/tests/auth/test_users_register_with_correct_roles.py
+++ backend/tests/auth/test_users_register_with_correct_roles.py
@@ -19,33 +19,15 @@ def test_user_can_register_as_worker(anon_api_client):
     )
 
     assert response.status_code == 201
-
-    user_url = reverse("users-detail", kwargs={"pk": "kabob15"})
-    user_response = anon_api_client.get(user_url)
-    assert user_response.data.get("roles") == [3]
-
-
-def test_user_was_registered_by_admin_as_reviewer(api_client_admin):
-
-    url = reverse("register-user")
-
-    api_client_admin
-    response = api_client_admin.post(
-        url,
-        {
-            "email": "wasdaqwe@gmail.com",
-            "username": "kabob16",
-            "password": "somepassidk",
-            "first_name": "ahmud",
-            "last_name": "bahmut",
-        },
+    token_url = reverse("jwt-obtain-token")
+    resp = anon_api_client.post(
+        token_url,
+        {"username": "kabob15", "password": "somepassidk"},
     )
+    assert resp.status_code == 200
 
-    assert response.status_code == 201
-
-    get_user_url = reverse("users-detail", kwargs={"pk": "kabob16"})
-    response_single_user = api_client_admin.get(get_user_url)
-
-    # users detail view was found
-    assert response_single_user.status_code == 200
-    assert response_single_user.data.get("roles") == [2, 3]
+    anon_api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {resp.data.get('access')}")
+    user_url = reverse("worker-detail-username", kwargs={"username": "kabob15"})
+    user_response = anon_api_client.get(user_url)
+    print(user_response.data)
+    assert user_response.data.get("roles")[0]["name"] == "Worker"
