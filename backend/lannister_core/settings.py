f517fb7b60521cea4755eef42acf0ef26d86a422
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-29 09:11:48 +0300

    Add backend cors config

--- backend/lannister_core/settings.py
+++ backend/lannister_core/settings.py
@@ -39,16 +39,27 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_spectacular",
+    "corsheaders",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://0.0.0.0:80",
+    "http://18.184.0.151:80",
+    "http://18.184.0.151",
+    "http://localhost:8000",
+    "http://localhost:8001",
 ]
 
 ROOT_URLCONF = "lannister_core.urls"
